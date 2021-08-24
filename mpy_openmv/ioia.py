b4b302a5ea006e3a6743eef885100e71cf7acb0b
Dawid Szewc <44799359+Xephen20@users.noreply.github.com>
2021-08-24 11:19:22 +0200

    Update ioia.py

--- mpy_openmv/ioia.py
+++ mpy_openmv/ioia.py
@@ -180,3 +180,59 @@ class Velux():
             sub['y']=od_y
             sub['angle']=angle
         return sub
+
+    def metal2(self, img, threshold=(176, 255), close=2, ratio = 0.73, min_area= 5000, max_area=50000):
+
+        img.binary([threshold])
+        if close !=0:
+            img.close(close)
+        sub = {}
+        for b in img.find_blobs([threshold], pixels_threshold = 100, area_threshold = 4000):
+            if b.area() > min_area and b.area() < max_area:
+                l = b.major_axis_line()
+                angle = self.math.atan2(l[1] - l[3], l[0] - l[2])
+                if self.distance(b.cx(), b.cy(), l[0], l[1]) > self.distance(b.cx(), b.cy(), l[2], l[3]):
+                    angle = angle + self.math.radians(180)
+
+                angle = angle - self.math.radians(90)
+                c2x = int(b.cx() + 8 * self.math.cos(angle))
+                c2y = int(b.cy() + 8 * self.math.sin(angle))
+
+                angle = angle + self.math.radians(270)
+                c3x = int(c2x + 124 * self.math.cos(angle))
+                c3y = int(c2y + 124 * self.math.sin(angle))
+
+                angle = self.math.degrees(angle)
+                if angle < 0:
+                    angle = angle + 360
+                if angle > 360:
+                    angle = angle - 360
+                angle = angle - 180
+
+                roi = c3x - 1, c3y - 1, 1, 1
+
+                try:
+                    roi = img.get_statistics(thresholds = [threshold], roi = roi)[0]
+                except OSError:
+                    pass
+
+                if roi == 0:
+                    img.draw_string(int(b.cxf() + 20), int(b.cyf() + 20), "Correct position", (255, 255, 255))
+                    img.draw_string(int(b.cxf() + 5), int(b.cyf() + 50), "X:%s "%int(b.cxf())+"Y:%s "%int(b.cyf())+"an:%s "%int(angle), (0,255,0), scale = 2)
+                    img.draw_rectangle(b[0:4], color=(255, 255, 255))
+                    img.draw_string(int(b.cxf() + 20), int(b.cyf() + 20), "%s"%roi, (255,0,0), scale = 2)
+                    #        w    h
+                    shape = 800, 600
+                    center = shape[0]/2, shape[1]/2
+
+                    # Oś X
+                    od_x = (b.cxf() - center[0]) * ratio
+
+                    # Oś Y
+                    od_y = (b.cyf() - center[1]) * ratio
+
+
+                    sub['x']=od_x
+                    sub['y']=od_y
+                    sub['angle']=angle
+        return sub
