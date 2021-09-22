45a8e368995f4f397195434fb1fe7e2f35873982
Dawid Szewc <44799359+Xephen20@users.noreply.github.com>
2021-09-22 09:46:21 +0200

    Added Olimex
    Logs from olimex on webserver

---
+++ mpy_olimex/logs.py
@@ -0,0 +1,68 @@
+import picoweb
+import network
+import machine
+import sys
+import ulogging as logging
+import time
+
+net = network.LAN()
+net.active(True)
+net.ifconfig(('192.168.125.89', '255.255.255.0', '192.168.125.1', '8.8.8.8'))
+log = []
+log.append("Net address: %s"%str(net.ifconfig()))
+
+app = picoweb.WebApp(__name__)
+
+@app.route("/")
+def index(req, resp):
+    yield from picoweb.start_response(resp)
+    yield from resp.awrite("%s"%log[0])
+    yield from resp.awrite(b"""\
+    <html>
+        <head>
+        </head>
+
+        <body>
+            
+        </body>
+    </head
+    """)
+
+@app.route("/restart")
+def index(req, resp):
+    if req.method == "POST":
+        yield from req.read_form_data()
+    else:
+        req.parse_qs()
+    machine.reset()
+
+@app.route("/poweroff")
+def index(req, resp):
+    if req.method == "POST":
+        yield from req.read_form_data()
+    else:
+        req.parse_qs()
+    sys.exit()
+
+@app.route("/ledon")
+def index(req, resp):
+    if req.method == "POST":
+        yield from req.read_form_data()
+    else:
+        req.parse_qs()
+    for x in range(50):
+        pyb.LED(1).on()
+    	time.sleep(0.1)
+    	pyb.LED(1).off()
+        time.sleep(0.1)
+
+@app.route("/ledoff")
+def index(req, resp):
+    if req.method == "POST":
+        yield from req.read_form_data()
+    else:
+        req.parse_qs()
+    pyb.LED(1).off()
+
+logging.basicConfig(level=logging.INFO)
+app.run(debug=True, host="192.168.125.89")
