fb87ce068b153d05727fe118e953cf97440646ac
Daniel Gorny <reverbrick@interia.pl>
2021-11-02 09:32:05 +0100

    eod

--- mpy_openmv/html.py
+++ mpy_openmv/html.py
@@ -1,21 +1,32 @@
-import ubinascii, json
+import ubinascii, json, omv
 from machine import UART
-from ioia import Camera, Flash, Velux; import time
-from comms import recv_msg, send_msg
-cam = Camera(exposure=50000, framesize="QQQVGA")
-cam.sensor.set_quality(100)
-fla = Flash(100,0)
-cli = Velux()
+import time, sensor
+omv.disable_fb(True)
+sensor.reset()
+sensor.set_pixformat(sensor.GRAYSCALE)
+sensor.set_framesize(sensor.SXGA)
+sensor.skip_frames(time = 2000)
 ser = UART(3, 921600)
 
-resp = b"""HTTP/1.1 200 OK\r\nContent-type: text/html\r\n\r\n"""
-html = b"""<img src="data:image/jpeg;base64, %s"/>"""
+resp = b"""HTTP/1.1 200 OK\r\nConnection: Close\r\nContent-type: %s\r\nContent-Length: %s\r\n\r\n%s\x04"""
 
 while(True):
-    req = ser.read(4096)
+    req = ser.readline()
     if req:
-        if b"GET" in req:
-            img = cam.snap(fla, corr=0.9)
-            ser.write(resp)
-            ser.write(html%ubinascii.b2a_base64(img.compress(quality=90)))
+        if b"GET /img" in req:
+            img = sensor.snapshot().copy(x_size=640, copy_to_fb=True).compress(quality=50)
+            data = ubinascii.a2b_base64((ubinascii.b2a_base64(img)))
+            ser.write(resp%("image/jpeg",len(data),data))
+        elif b"GET /roi" in req:
+            img = sensor.snapshot().copy(roi=(320,256,640,512), copy_to_fb=True).compress(quality=50)
+            data = ubinascii.a2b_base64((ubinascii.b2a_base64(img)))
+            ser.write(resp%("image/jpeg",len(data),data))
+        elif b"GET /data" in req:
+            data = [{"foo": 1, "bar": 2.005}]
+            data = json.dumps(data)
+            ser.write(resp%("application/json",len(data),data))
+        elif b"GET /" in req:
+            with open("index.html") as index:
+                data = index.read()
+                ser.write(resp%("text/html",len(data),data))
     time.sleep(0.1)
