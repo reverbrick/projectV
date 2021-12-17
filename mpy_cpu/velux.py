966ad3965ed6ade56af3e5a291606413cc7d0c6c
Daniel Górny <reverbrick@interia.pl>
2021-12-17 15:56:05 +0100

    eod

--- mpy_cpu/velux.py
+++ mpy_cpu/velux.py
@@ -1,4 +1,5 @@
 #Proudly brought to you by IOIA/ReverbLand. For support please email Daniel Górny at dadmin.dgor@gmail.com
+#refer to states.txt for state machine explanations
 import time, socket, machine
 from libs.camera import H7c
 from libs.stepper import Nanotec
@@ -16,13 +17,32 @@ s.listen(5)
 print("ready")
 scara = {}
 
+def test():
+    while True:
+        bowls[0].prog()
+        bowls[1].prog()
+        #print(0,getItems(0))
+        #print(1,getItems(1))
+        #print(shake(1))
+        time.sleep(0.7)
+
 def shake(id, sleep=0.4):
+    """
+    ret = []
+    retry = 1
+    while ret==[]:
+        print("retry #%s"%retry)
+        bowls[id].prog()
+        time.sleep(sleep)
+        ret = cams[id].get()
+    """
     bowls[id].prog()
     time.sleep(sleep)
     ret = cams[id].get()
     return ret
 
 def getItems(id):
+    #initial shaking
     if items[id] == []:
         items[id] = shake(id)
     #pick one from the list
@@ -39,31 +59,35 @@ def getItems(id):
     if pos!={}:
         return "'%s''%s''%s''%s'\r\n"%(pos["x"], pos["y"], pos["angle"], pos["side"])
     else:
-        #return "'0''0''0''-1'\r\n"
-        return "''''''''\r\n"
+        return "'0''0''0''-1'\r\n"
+        #return "''''''''\r\n"
 
 while(True):
     c, addr = s.accept()
     print("connected %s"%addr[0])
     scara[addr[0]] = c.makefile()
-    try:
-        message = scara[addr[0]].recv(1024)
-        print(message)
-        if message == b'\x00\x07snap0\r':
-            items = [[],[]]
-            scara[addr[0]].write("'0''0''0''0'\r\n")
-        elif message == b'\x00\x07snap1\r':
-            scara[addr[0]].write(getItems(0))
-        elif message == b'\x00\x07snap2\r':
-            scara[addr[0]].write(getItems(1))
-        #wait till state change
-        retry = 0
-        while "state=3" in str(scara[addr[0]]):
-            print(".", end="")
-            retry=retry+1
-            time.sleep_ms(10)
-            if retry>20:
-                break
-        c.close()
-    except:
-        machine.reset
+    #try:
+    message = scara[addr[0]].recv(1024)
+    print(message)
+    if message == b'\x00\x07snap0\r':
+        items = [[],[]]
+        scara[addr[0]].write("'0''0''0''0'\r\n")
+    elif message == b'\x00\x07snap1\r':
+        scara[addr[0]].write(getItems(0))
+    elif message == b'\x00\x07snap2\r':
+        scara[addr[0]].write(getItems(1))
+    """
+    #wait till state change
+    retry = 0
+    while "state=3" in str(scara[addr[0]]):
+        print(scara[addr[0]])
+        print(".", end="")
+        retry=retry+1
+        time.sleep_ms(30)
+        if retry>20:
+            break
+    """
+    scara[addr[0]].recv(1024) 
+    c.close()
+    #except:
+    #    machine.reset
---
+++ nanoj/progs/metal_velo2/metal_shake.cpp
@@ -0,0 +1,28 @@
+map U16 StatusWord as input 0x6041:00
+map U32 Outputs as output 0x60FE:01
+#include "wrapper.h"
+#include "ioia.h"
+
+void user()
+{
+	ClearDigitalOutput(2);
+	mode_velocity();
+	start();
+	ClearDigitalOutput(1);
+	sleep(40);
+	SetDigitalOutput(1);
+	sleep(100);
+	ClearDigitalOutput(1);
+	sleep(40);	
+	SetDigitalOutput(1);
+	sleep(100);
+	ClearDigitalOutput(1);
+	sleep(40);	
+	SetDigitalOutput(1);
+	sleep(100);
+	od_write(0x6040, 0x00, 0x0);
+	sleep(1500);
+	SetDigitalOutput(2);
+	//stop();
+	od_write(0x2300, 0x00, 0x0);
+}
